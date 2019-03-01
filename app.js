0bb2cb05cfa54c83eda67d642198b301f9b7bea2
Jacob Bennett <kf7ags@gmail.com>
2019-03-01 10:40:34 -0700

    can now add users to a JSON file

--- app.js
+++ app.js
@@ -3,12 +3,49 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const bodyParser = require('body-parser');
+const userFile = require('./users/usersFile');
 
 const port = 3000;
+let numUsers;
+if(userFile.numUsers){
+    numUsers = userFile.numUsers;
+}
+else{
+    numUsers = 0;
+}
 
 
 app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('views', path.join(__dirname, 'pugViews'));
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, resp)=>{
+    resp.render('index');
+});
+app.get('/users', (req, resp)=>{
+    
+});
+
+app.post('/addUser', (req, resp)=>{
+    numUsers++;
+    let userData = req.body;
+    userData.uid = `${userData.userName}${numUsers}`;
+    let newJSON = {
+        numUsers: numUsers,
+        users: userFile.users
+    };
+    newJSON.users.push(userData);
+    console.log(newJSON);
+    fs.writeFile('./users/usersFile.json', JSON.stringify(newJSON), (err)=>{
+        if (err) throw err;
+        resp.redirect('/users');
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`);
