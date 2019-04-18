0b0bcceaebbdd9bb6c581a1ca4cb3f2badda0652
JerecTharen <kf7ags@gmail.com>
2019-04-18 10:12:45 -0600

    more database method updating

--- app.js
+++ app.js
@@ -40,58 +40,42 @@ app.get('/', (req, resp)=>{
     resp.render('index');
 });
 app.get('/users', (req, resp)=>{
-    console.log('in users');
+    // console.log('in users');
     user.find({}, (err, data)=>{
         if(err) return console.log(`Opps: ${err}`);
         // console.log(`data -- ${JSON.stringify(data)}`);
+        // console.log(data[0]._id);
         resp.render('users', {allUsers: data});
     });
 });
 app.get('/users/:uid', (req, resp)=>{
-    let user;
-    let found = false;
-    for(let i = 0; i < userFile.users.length; i++){
-        // console.log(userFile.users[i]);
-        if(userFile.users[i].uid===req.params.uid){
-            found = true;
-            user = userFile.users[i];
-            // console.log(user);
-            resp.render('userEdit', {uid: req.params.uid, user: user});
+    user.findById(req.params.uid, (err, data)=>{
+        if(data){
+            resp.render('userEdit', {uid: req.params.uid, user: data});
         }
-    }
-    if(!found){
-        resp.send('We could not find that user');
-    }
+        else{
+            resp.send('We could not find that user');
+        }
+    });
+
 });
 app.get('/delete/:uid', (req, resp)=>{
-    let newJSON = {
-        numUsers: numUsers,
-        users: userFile.users
-    };
-    for(let i = 0; i<newJSON.users.length; i++){
-        if(newJSON.users[i].uid === req.params.uid){
-            newJSON.users.splice(i, 1);
-        }
-    }
-    console.log(newJSON);
-    fs.writeFile('./users/usersFile.json', JSON.stringify(newJSON), (err)=>{
-        if (err) throw err;
+    user.deleteOne({_id: req.param.uid}, (err)=>{
+        if(err) return console.error(err);
         resp.redirect('/users');
     });
 });
 
 app.post('/addUser', (req, resp)=>{
-    numUsers++;
-    let userData = req.body;
-    userData.uid = `${userData.userName}${numUsers}`;
-    let newJSON = {
-        numUsers: numUsers,
-        users: userFile.users
-    };
-    newJSON.users.push(userData);
-    console.log(newJSON);
-    fs.writeFile('./users/usersFile.json', JSON.stringify(newJSON), (err)=>{
-        if (err) throw err;
+    let newUser = new user({
+        userName: req.body.userName,
+        email: req.body.email,
+        age: req.body.age.toString()
+    });
+    // let userData = req.body;
+    newUser.save((err, addedUser)=>{
+        if(err) return console.error(err);
+        console.log(addedUser.userName + ' saved to user collection');
         resp.redirect('/users');
     });
 });
